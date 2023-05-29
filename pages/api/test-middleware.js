a202ca26237a82ee13a5e7e8573209b080ffde36
luanavfg <luanavefago1@gmail.com>
2023-05-29 10:58:11 -0300

    feat: class 05 - add middleware to validate auth

---
+++ pages/api/test-middleware.js
@@ -0,0 +1,12 @@
+function handler(req, res) {
+  res.status(200).json({ message: "OK!" });
+}
+const withAuthentication = (handlerFn) => (req, res) => {
+  if (!req.headers.authentication) {
+    res.status(401).json({ message: "ERROR" });
+  }
+
+  return handlerFn(req, res);
+};
+
+export default withAuthentication(handler);
